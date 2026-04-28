1b97aa1d43a226981cf680e072b8ec9bd3d5fcb2
Singorenko <cbaezenko@gmail.com>
2026-04-28 06:03:35 +0000

    feat(team): add TeamDetailRequestSerializer and implement get_team_details_service_repository

--- team/domain/services/team_service.py
+++ team/domain/services/team_service.py
@@ -1,8 +1,15 @@
 from core.exceptions.bd import RepositoryError
-from core.exceptions.domain import PlayerNotFoundError
+from core.exceptions.domain import PlayerNotFoundError, TeamNotFoundError
 from team.infrastructure.repositories.team_write_repository import (
     create_team_service_repository,
 )
+from team.infrastructure.repositories.team_read_repository import (
+    get_team_details_service_repository,
+)
+
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def create_team_service(
@@ -21,4 +28,15 @@ def create_team_service(
     except PlayerNotFoundError:
         raise
     except RepositoryError:
+        raise
+
+
+def get_team_details_service(team_name):
+    try:
+        return get_team_details_service_repository(team_name=team_name)
+    except TeamNotFoundError:
+        logger.exception(TeamNotFoundError.default_detail)
+        raise
+    except RepositoryError:
+        logger.exception(RepositoryError.default_detail)
         raise
